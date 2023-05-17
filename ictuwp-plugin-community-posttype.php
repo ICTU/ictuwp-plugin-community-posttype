07d52b603b5738b605b670743d8a0ae8d4721fc5
Paul van Buuren <paul@wbvb.nl>
2023-05-17 16:24:38 +0200

    first draft for creating posttype and taxonomy

---
+++ ictuwp-plugin-community-posttype.php
@@ -0,0 +1,254 @@
+<?php
+
+/**
+ * @link                https://github.com/ICTU/ictuwp-plugin-community-posttype
+ * @package             ictuwp-plugin-community-posttype
+ *
+ * @wordpress-plugin
+ * Plugin Name:         ICTU / Digitale Overheid / Community
+ * Plugin URI:          https://github.com/ICTU/ictuwp-plugin-community-posttype
+ * Description:         Plugin voor het aanmaken van posttype 'community' en bijbehorende taxonomieen.
+ * Version:             0.0.1
+ * Version description: Initial version.
+ * Author:              Paul van Buuren
+ * Author URI:          https://github.com/ICTU/ictuwp-plugin-community-posttype/
+ * License:             GPL-2.0+
+ * License URI:         http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:         wp-rijkshuisstijl
+ * Domain Path:         /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+//========================================================================================================
+
+// Dutch slug for taxonomy
+$slug = 'communityxx';
+
+if ( get_bloginfo( 'language' ) !== 'nl-NL' ) {
+	// non Dutch slug for taxonomy
+	$slug = 'community';
+}
+
+
+define( 'DO_COMMUNITY_CPT', $slug );
+define( 'DO_COMMUNITYTYPE_CT', 'type_community' );
+
+defined( 'DO_COMMUNITY_OVERVIEW_TEMPLATE' ) or define( 'DO_COMMUNITY_OVERVIEW_TEMPLATE', 'template-overview-communities.php' );
+defined( 'DO_COMMUNITY_DETAIL_TEMPLATE' ) or define( 'DO_COMMUNITY_DETAIL_TEMPLATE', 'template-community-detail.php' );
+
+//========================================================================================================
+add_action( 'plugins_loaded', array( 'DO_COMMUNITY_CPT', 'init' ), 10 );
+
+
+//========================================================================================================
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    0.0.1
+ */
+
+
+if ( ! class_exists( 'DO_COMMUNITY_CPT' ) ) :
+
+	class DO_COMMUNITY_CPT {
+
+		/** ----------------------------------------------------------------------------------------------------
+		 * Init
+		 */
+		public static function init() {
+
+			$newtaxonomy = new self();
+
+		}
+
+		/** ----------------------------------------------------------------------------------------------------
+		 * Constructor
+		 */
+		public function __construct() {
+
+			$this->fn_ictu_community_setup_actions();
+
+		}
+
+		/** ----------------------------------------------------------------------------------------------------
+		 * Hook this plugins functions into WordPress.
+		 * Use priority = 20, to ensure that the taxonomy is registered for post types from other plugins,
+		 * such as the podcasts plugin (seriously-simple-podcasting)
+		 */
+		private function fn_ictu_community_setup_actions() {
+
+			add_action( 'init', array( $this, 'fn_ictu_community_register_posttypes' ), 20 );
+
+			// add page templates
+//			add_filter( 'template_include', array( $this, 'fn_ictu_community_append_template_locations' ) );
+
+			// filter the breadcrumbs
+//			add_filter( 'wpseo_breadcrumb_links', array( $this, 'fn_ictu_community_yoast_filter_breadcrumb' ) );
+
+		}
+
+		/** ----------------------------------------------------------------------------------------------------
+		 * Do actually register the post types we need
+		 *
+		 * @return void
+		 */
+		public function fn_ictu_community_register_posttypes() {
+
+			require_once plugin_dir_path( __FILE__ ) . 'includes/register-community-posttype.php';
+
+		}
+
+
+		/**
+		 * Checks if the template is assigned to the page
+		 *
+		 * @in: $template (string)
+		 *
+		 * @return: $template (string)
+		 *
+		 */
+		public function fn_ictu_community_append_template_locations( $template ) {
+
+			// Get global post
+			global $post;
+			$file       = '';
+			$pluginpath = plugin_dir_path( __FILE__ );
+
+
+			if ( $post ) {
+				// Do we have a post of whatever kind at hand?
+				// Get template name; this will only work for pages, obviously
+				$page_template = get_post_meta( $post->ID, '_wp_page_template', true );
+
+				if ( ( DO_COMMUNITY_OVERVIEW_TEMPLATE === $page_template ) || ( DO_COMMUNITY_DETAIL_TEMPLATE === $page_template ) ) {
+					// these names are added by this plugin, so we return
+					// the actual file path for this template
+					$file = $pluginpath . $page_template;
+				} else {
+					// exit with the already set template
+					return $template;
+				}
+
+			} else {
+				// Not a post, not a term, return the template
+				return $template;
+			}
+
+			// Just to be safe, check if the file actually exists
+			if ( $file && file_exists( $file ) ) {
+				return $file;
+			} else {
+				// o dear, who deleted the file?
+				echo $file;
+			}
+
+			// If all else fails, return template
+			return $template;
+		}
+
+
+		/**
+		 * Filter the Yoast SEO breadcrumb
+		 *
+		 * @in: $links (array)
+		 *
+		 * @return: $links (array)
+		 *
+		 */
+		public function fn_ictu_community_yoast_filter_breadcrumb( $links ) {
+
+			// exceptions here
+
+			return $links;
+
+		}
+
+		/**
+		 * Retrieve a page that is the overview page. This
+		 * page shows all available items.
+		 *
+		 * @in: $args (array)
+		 *
+		 * @return: $overview_page_id (integer)
+		 *
+		 */
+
+		private function fn_ictu_community_get_thema_overview_page( $args = array() ) {
+
+			$return = 0;
+
+			// TODO: discuss if we need to make this page a site setting
+			// there is no technical way to limit the number of pages with
+			// template DO_COMMUNITY_OVERVIEW_TEMPLATE, so the page we find may not be
+			// the exact desired page for in the breadcrumb.
+			//
+			// Try and find 1 Page
+			// with the DO_COMMUNITY_OVERVIEW_TEMPLATE template...
+			$page_template_query_args = array(
+				'number'      => 1,
+				'sort_column' => 'post_date',
+				'sort_order'  => 'DESC',
+				'meta_key'    => '_wp_page_template',
+				'meta_value'  => DO_COMMUNITY_OVERVIEW_TEMPLATE
+			);
+			$overview_page            = get_pages( $page_template_query_args );
+
+			if ( $overview_page && isset( $overview_page[0]->ID ) ) {
+				$return = $overview_page[0]->ID;
+			}
+
+			return $return;
+
+		}
+
+
+	}
+
+endif;
+
+
+//========================================================================================================
+
+/**
+ * Load plugin textdomain.
+ * only load translations if we can safely assume the taxonomy is active
+ */
+add_action( 'init', 'fn_ictu_community_load_plugin_textdomain' );
+
+function fn_ictu_community_load_plugin_textdomain() {
+
+	load_plugin_textdomain( 'wp-rijkshuisstijl', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+}
+
+//========================================================================================================
+
+/**
+ * Returns array of allowed page templates
+ *
+ * @return array with extra templates
+ */
+function fn_ictu_community_add_templates() {
+
+	$return_array = array(
+		DO_COMMUNITY_OVERVIEW_TEMPLATE => _x( '[Community] alle community\'s', 'label page template', 'wp-rijkshuisstijl' ),
+		DO_COMMUNITY_DETAIL_TEMPLATE   => _x( '[Community] community-detail', 'label page template', 'wp-rijkshuisstijl' )
+	);
+
+	return $return_array;
+
+}
+
+//========================================================================================================
+
+
+
---
+++ includes/register-community-posttype.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Custom Post Type: Community
+ * Custom Taxonomies: Communitytype, Communitygrootte
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_taxonomy/
+ * @see https://developer.wordpress.org/reference/functions/get_taxonomy_labels/
+ *
+ * ----------------------------------------------------- */
+
+
+// ---------------------------------------------------------------------------------------------------
+$slug_of_posttype = DO_COMMUNITY_CPT;
+
+$args = array(
+	'label'               => esc_html__( DO_COMMUNITY_CPT, 'wp-rijkshuisstijl' ),
+	'description'         => '',
+	'labels'              => array(
+		'name'                  => esc_html_x( 'Community\'s', 'post type', 'wp-rijkshuisstijl' ),
+		'singular_name'         => esc_html_x( 'Community', 'post type', 'wp-rijkshuisstijl' ),
+		'menu_name'             => esc_html_x( 'Community\'s', 'post type', 'wp-rijkshuisstijl' ),
+		'name_admin_bar'        => esc_html_x( 'Community', 'post type', 'wp-rijkshuisstijl' ),
+		'archives'              => esc_html_x( 'Overzicht community\'s', 'post type', 'wp-rijkshuisstijl' ),
+		'attributes'            => esc_html_x( 'Eigenschappen community', 'post type', 'wp-rijkshuisstijl' ),
+		'parent_item_colon'     => esc_html_x( 'Parent Map:', 'post type', 'wp-rijkshuisstijl' ),
+		'all_items'             => esc_html_x( 'Alle community\'s', 'post type', 'wp-rijkshuisstijl' ),
+		'add_new_item'          => esc_html_x( 'Community toevoegen', 'post type', 'wp-rijkshuisstijl' ),
+		'add_new'               => esc_html_x( 'Toevoegen', 'post type', 'wp-rijkshuisstijl' ),
+		'new_item'              => esc_html_x( 'Nieuw community', 'post type', 'wp-rijkshuisstijl' ),
+		'edit_item'             => esc_html_x( 'Bewerk community', 'post type', 'wp-rijkshuisstijl' ),
+		'update_item'           => esc_html_x( 'Update community', 'post type', 'wp-rijkshuisstijl' ),
+		'view_item'             => esc_html_x( 'Bekijk community', 'post type', 'wp-rijkshuisstijl' ),
+		'view_items'            => esc_html_x( 'Bekijk community\'s', 'post type', 'wp-rijkshuisstijl' ),
+		'search_items'          => esc_html_x( 'Zoek community', 'post type', 'wp-rijkshuisstijl' ),
+		'not_found'             => esc_html_x( 'Not found', 'post type', 'wp-rijkshuisstijl' ),
+		'not_found_in_trash'    => esc_html_x( 'Not found in Trash', 'post type', 'wp-rijkshuisstijl' ),
+		'featured_image'        => esc_html_x( 'Featured Image', 'post type', 'wp-rijkshuisstijl' ),
+		'set_featured_image'    => esc_html_x( 'Set featured image', 'post type', 'wp-rijkshuisstijl' ),
+		'remove_featured_image' => esc_html_x( 'Remove featured image', 'post type', 'wp-rijkshuisstijl' ),
+		'use_featured_image'    => esc_html_x( 'Use as featured image', 'post type', 'wp-rijkshuisstijl' ),
+		'insert_into_item'      => esc_html_x( 'Insert into Map', 'post type', 'wp-rijkshuisstijl' ),
+		'uploaded_to_this_item' => esc_html_x( 'Uploaded to this community', 'post type', 'wp-rijkshuisstijl' ),
+		'items_list'            => esc_html_x( 'Map list', 'post type', 'wp-rijkshuisstijl' ),
+		'items_list_navigation' => esc_html_x( 'Maps list navigation', 'post type', 'wp-rijkshuisstijl' ),
+		'filter_items_list'     => esc_html_x( 'Filter community list', 'post type', 'wp-rijkshuisstijl' ),
+	),
+	'supports'            => array( 'title', 'author', 'excerpt', 'editor', 'thumbnail' ),
+	'hierarchical'        => false,
+	'public'              => true,
+	'show_ui'             => true,
+	'show_in_menu'        => true,
+	'show_in_admin_bar'   => true,
+	'show_in_nav_menus'   => true,
+	'can_export'          => true,
+	'has_archive'         => true,
+	'exclude_from_search' => false,
+	'publicly_queryable'  => true,
+	'rewrite'             => array( 'slug' => $slug_of_posttype ),
+	'capability_type'     => 'post'
+);
+
+register_post_type( DO_COMMUNITY_CPT, $args );
+
+// Communitytype
+$labels = array(
+	'name'              => esc_html_x( 'Type community', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'singular_name'     => esc_html_x( 'Communitytype', 'taxonomy singular name', 'wp-rijkshuisstijl' ),
+	'search_items'      => esc_html_x( 'Search communitytype', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'all_items'         => esc_html_x( 'All communitytypes', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'parent_item'       => esc_html_x( 'Parent communitytype', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'parent_item_colon' => esc_html_x( 'Parent communitytype:', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'edit_item'         => esc_html_x( 'Edit communitytype', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'update_item'       => esc_html_x( 'Update communitytype', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'add_new_item'      => esc_html_x( 'Add new communitytype', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'new_item_name'     => esc_html_x( 'New communitytype name', 'taxonomy', 'wp-rijkshuisstijl' ),
+	'menu_name'         => esc_html_x( 'Communitytype', 'taxonomy', 'wp-rijkshuisstijl' ),
+);
+
+$args = array(
+	'hierarchical'      => true,
+	'labels'            => $labels,
+	'show_ui'           => true,
+	'show_admin_column' => true,
+	'query_var'         => true,
+	'rewrite'           => array( 'slug' => DO_COMMUNITYTYPE_CT ),
+);
+
+register_taxonomy( DO_COMMUNITYTYPE_CT, array( DO_COMMUNITY_CPT ), $args );
