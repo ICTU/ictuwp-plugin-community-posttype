9ea7fd884a8c524fcc01d3324a114eeb29ceca90
Paul van Buuren <paul@wbvb.nl>
2023-05-17 15:34:51 +0200

    empty templates

---
+++ template-community-detail.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Thema Term detail page
+ *
+ * This template is attached to a random page
+ * that has been assigned to a Thema taxonomy term as
+ * ACF `thema_taxonomy_page` Link field
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
---
+++ template-overview-communities.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Template Name: Template Thema's
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
